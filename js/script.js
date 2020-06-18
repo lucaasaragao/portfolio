65d8ca559b15e3a26ac6471b816e0602dd44dfb2
Lucas Aragão <lucas.magno@dcx.ufpb.br>
2020-06-18 00:52:07 -0300

    Arquivos primarios

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let textarea = document.querySelector(".textarea")
+testString = "Lucas@magno:$ ~ npm install portfólio  \n[**************************] successful  \nLucas@magno:$ ~ Olá eu sou o Lucas, obrigado por me instalar :) \nAjudo alguns designs, pequenas agências e algumas empresas a dar \nvida às suas ideias. Com tecnologias como ~Node ~React ~Django ~Spring\ncom uma boa ajuda do ~Stack Overflow e muito café no final tudo sempre\nda certo.\n\n\n \nLucas@magno:$ ~ Pressione qualquer tecla ..."
+
+var counter = 0;                   
+
+function WriteDefaultValuesTerminal () {           
+   setTimeout(function () {    
+    textarea.value += testString[counter]         
+      counter++;                     
+      if (counter < testString.length) {            
+         WriteDefaultValuesTerminal();            
+      }                       
+   }, 40)
+}
+WriteDefaultValuesTerminal();
+
+ 
+document.querySelector('body').addEventListener('keydown', function(event) {
+  window.location.href = 'projects.html';
+}); 
+
+
+
+
+
