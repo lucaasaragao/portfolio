4a3d4da6a66e59e3fe85ddf035333df1cd1c38d6
Lucas Aragão <lucas.magno@dcx.ufpb.br>
2020-07-21 19:20:49 -0300

    Modificações de fraseologia

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let textarea = document.querySelector(".textarea")
-testString = "Lucas@magno:$ ~ npm install portfólio  \n[**************************] successful  \nLucas@magno:$ ~ Olá eu sou o Lucas, obrigado por me instalar :) \nAjudo alguns designs, pequenas agências e algumas empresas a dar \nvida às suas ideias. Com tecnologias como ~Node ~React ~Django ~Spring\ncom uma boa ajuda do ~Stack Overflow e muito café no final tudo sempre\nda certo.\n\n\n \nLucas@magno:$ ~ Pressione qualquer tecla ..."
+testString = "Lucas@magno:$ ~ npm install portfólio  \n[**************************] successful  \nLucas@magno:$ ~ Olá, eu sou o Lucas. Obrigado por me instalar :) \nAjudo alguns designs, pequenas agências e algumas empresas a darem \nvida às suas ideias. Com tecnologias como ~Node ~React ~Django ~Spring\ncom uma boa ajuda do ~Stack Overflow e muito café no final tudo sempre\ndá certo.\n\n\n \nLucas@magno:$ ~ Pressione qualquer tecla ..."
 
 var counter = 0;                   
 
