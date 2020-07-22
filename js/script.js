df64f13c07efb81d22a4bcb9fe28c98323a23e94
Lucas Aragão <lucas.magno@dcx.ufpb.br>
2020-07-22 20:06:49 -0300

    Medias Queries

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let textarea = document.querySelector(".textarea")
-testString = "Lucas@magno:$ ~ npm install portfólio  \n[**************************] successful  \nLucas@magno:$ ~ Olá, eu sou o Lucas. Obrigado por me instalar :) \nAjudo alguns designs, pequenas agências e algumas empresas a darem \nvida às suas ideias. Com tecnologias como ~Node ~React ~Django ~Spring\ncom uma boa ajuda do ~Stack Overflow e muito café no final tudo sempre\ndá certo.\n\n\n \nLucas@magno:$ ~ Pressione qualquer tecla ..."
+testString = "Lucas@magno:$ ~ npm install portfólio  \n[**************************] successful  \nLucas@magno:$ ~ Olá, eu sou o Lucas. Obrigado por me instalar :) \nAjudo alguns designs, pequenas agências e algumas empresas a darem \nvida às suas ideias. Com tecnologias como ~Node ~React ~Django ~Spring\ncom uma boa ajuda do ~Stack Overflow e muito café no final tudo sempre\ndá certo.\n\n \nLucas@magno:$ ~ Pressione qualquer tecla ..."
 
 var counter = 0;                   
 
@@ -14,7 +14,7 @@ function WriteDefaultValuesTerminal () {
 }
 WriteDefaultValuesTerminal();
 
- 
+
 document.querySelector('body').addEventListener('keydown', function(event) {
    window.location.href = 'projects.html';
 }); 
